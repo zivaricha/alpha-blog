faa85dba7c27a56d6a4dcbc01913bab431e152fb
Ziv Aricha <zivaricha@gmail.com>
2017-11-19 15:11:34 +0000

    production.rb changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   
-  get 'pages/home', to: 'pages#home'
+  root 'pages#home'
   get 'pages/about', to: 'pages#about'
 
   # You can have the root of your site routed with "root"
