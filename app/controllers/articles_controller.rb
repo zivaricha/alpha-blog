a850844689d0729ed2f5db8ca0b0ad3081af0415
Ziv Aricha <zivaricha@gmail.com>
2017-11-20 10:51:08 +0000

    added show view, and display errors if occured

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,19 +1,24 @@
 class ArticlesController < ApplicationController
  
- def new
-   @article = Article.new
- end
- 
- def create
-  @article = Article.new(article_params)
-  if @article.save
-   redirect_to article_pat(@article)
-  else
-   render 'new', notice: "was unable to save the article"
+  def new
+    @article = Article.new
   end
- end
- 
- def article_params
-  params.require(:article).permit(:title, :description)
- end
+  
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "The article was succefully saved."
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+    @article = Article.find(params[:id])
+  end
+  
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
