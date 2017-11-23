65210093eeacc5d2ea20d723391674002a3a08ab
Ziv Aricha <zivaricha@gmail.com>
2017-11-23 12:58:03 +0000

    added user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+  validates :username, presence: true, 
+                       uniqueness: { case_sensitive: false },
+                       length: { minimum: 3, maximum: 10 }
+  validates :email, presence: true, 
+                    uniqueness: { case_sensitive: false },
+                    length: { maximum: 105 },
+                    format: { with: VALID_EMAIL_REGEX }
+
 end
