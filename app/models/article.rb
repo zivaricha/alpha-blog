3b866ca0dadf7974a6930c615c57da6a6ac8111a
Ziv Aricha <zivaricha@gmail.com>
2017-11-26 12:03:26 +0000

    added user article relations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ActiveRecord::Base
+  belongs_to :user
+
   validates :title, presence: true, length: { minimum: 3, maximum: 10}
   validates :description, presence: true, length: { minimum: 10, maximum: 300}
   
