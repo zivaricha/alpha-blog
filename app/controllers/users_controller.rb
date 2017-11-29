93453d8401b541e47696e5127ed343785e81a59f
Ziv Aricha <zivaricha@gmail.com>
2017-11-29 11:29:28 +0000

    added User show view and gravatar image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+   before_action :find_user, only: [:edit, :update, :show, :destroy]
+   
   def new
     @user = User.new
   end
@@ -13,9 +15,28 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      flash[:success] = "User was succefully updated"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+  
+  def show
+  end
+  
   private
   
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+  
+  def find_user
+    @user = User.find(params[:id])
   end
 end
