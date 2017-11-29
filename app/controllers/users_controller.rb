b05a54209d57f8aaa8df0f8d07e024e32cf78fe8
Ziv Aricha <zivaricha@gmail.com>
2017-11-29 21:38:18 +0000

    Pagination for articles and users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :find_user, only: [:edit, :update, :show, :destroy]
   
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -32,6 +32,7 @@ class UsersController < ApplicationController
   end
   
   def show
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 1)
   end
   
   private
