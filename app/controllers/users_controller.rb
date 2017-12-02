cc7c7cba937f387e3da3cabb226874f763b6d68e
Ziv Aricha <zivaricha@gmail.com>
2017-12-02 16:28:18 +0000

    Login after signup and nav bar improvement

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the Alpha blog #{@user.username}"
       redirect_to articles_path
     else
