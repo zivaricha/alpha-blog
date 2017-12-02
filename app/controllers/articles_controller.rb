e6a4686b33fd585ed57b40fc8776603d5a654562
Ziv Aricha <zivaricha@gmail.com>
2017-12-02 16:20:59 +0000

    fixing signup

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :find_article, only: [:edit, :update, :show, :destroy]
-  before_action :require_user, expect: [:index, :show]
+  before_action :require_user, except: [:index, :show]
   before_action :require_same_user, only: [:edit, :update, :destroy]
   
   def index
