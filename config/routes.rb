6440c1a909071088bd54e18da3e460361e811b19
Ziv Aricha <zivaricha@gmail.com>
2017-12-04 14:01:26 +0000

    add categories model and tests

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
   
   delete 'logout', to: 'sessions#destroy'
   
+  resources :categories, except: [:destroy]
+  
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
