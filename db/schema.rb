1d73b17b6b7fe418528cc428ec90602ce8c36113
Ziv Aricha <zivaricha@gmail.com>
2017-12-04 15:24:38 +0000

    added article_categories table and association

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171204100541) do
+ActiveRecord::Schema.define(version: 20171204144324) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer  "article_id"
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
